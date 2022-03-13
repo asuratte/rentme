a00dcc4adcfc5b7ac2d417129e2d41f0dc7a6993
Michael Bastarache <mb00147@my.westga.edu>
2022-03-13 13:40:36 -0400

    Removed unused using statements

--- RentMe/Model/Employee.cs
+++ RentMe/Model/Employee.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RentMe.Model
+﻿namespace RentMe.Model
 {
     /// <summary>
     /// Defines a model class for an employee.
