360c1057301f1ace679148dc8cd4e6259a23bb2a
Michael Bastarache <mb00147@my.westga.edu>
2022-03-13 13:32:48 -0400

    Created controller and DAL for Employees.
    Added methods to validate login credentials and get an employee by username.

---
+++ RentMe/Controller/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using RentMe.DAL;
+using RentMe.Model;
+
+namespace RentMe.Controller
+{
+    /// <summary>
+    /// Defines a controller class that mediates between the EmployeeDAL and the view.
+    /// </summary>
+    public class EmployeeController
+    {
+        private readonly EmployeeDAL employeeDAL;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmployeeController"/> class.
+        /// </summary>
+        public EmployeeController()
+        {
+            this.employeeDAL = new EmployeeDAL();
+        }
+
+        /// <summary>
+        /// Checks the login credentials of the employee.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>True if credentials are valid.</returns>
+        public bool CheckLoginCredentials(string username, string password)
+        {
+            return this.employeeDAL.CheckLoginCredentials(username, password);
+        }
+
+        /// <summary>
+        /// Gets the employee by username.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <returns>Employee with the specified username.</returns>
+        public Employee GetEmployeeByUsername(string username)
+        {
+            return this.employeeDAL.GetEmployeeByUsername(username);
+        }
+    }
+}
---
+++ RentMe/DAL/EmployeeDAL.cs
@@ -0,0 +1,96 @@
+﻿using RentMe.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentMe.DAL
+{
+    /// <summary>
+    /// The DAL that accesses employee data in the DB.
+    /// </summary>
+    public class EmployeeDAL
+    {
+        /// <summary>
+        /// Checks the login credentials of the employee.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>True if valid credentials.</returns>
+        public bool CheckLoginCredentials(string username, string password)
+        {
+            int count;
+
+            string selectStatement =
+                "SELECT COUNT(*) " +
+                "FROM employee " +
+                "WHERE username = @username AND password = @password ";
+
+            using (SqlConnection connection = RentMeDBConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.Add("@username", System.Data.SqlDbType.NVarChar);
+                    selectCommand.Parameters["@username"].Value = username;
+
+                    selectCommand.Parameters.Add("@password", System.Data.SqlDbType.NVarChar);
+                    selectCommand.Parameters["@password"].Value = password;
+
+                    count = Convert.ToInt32(selectCommand.ExecuteScalar());
+                }
+            }
+            if (count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the employee by username.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <returns>Employee with specified username.</returns>
+        public Employee GetEmployeeByUsername(string username)
+        {
+            Employee employee = null;
+
+            string selectStatement =
+                "SELECT firstName, lastName, username " +
+                "FROM employee " +
+                "WHERE username = @username";
+
+            using (SqlConnection connection = RentMeDBConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.Add("@username", System.Data.SqlDbType.NVarChar);
+                    selectCommand.Parameters["@username"].Value = username;
+
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            employee = new Employee()
+                            {
+                                FirstName = reader["firstName"].ToString(),
+                                LastName = reader["lastName"].ToString(),
+                                Username = reader["username"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+            return employee;
+        }
+    }
+}
