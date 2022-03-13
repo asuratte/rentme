f7b75a84d76920b80c3fffc4730dd36735f03775
Michael Bastarache <mb00147@my.westga.edu>
2022-03-13 13:31:13 -0400

    Created model Employee class with properties for name and username.

---
+++ RentMe/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentMe.Model
+{
+    /// <summary>
+    /// Defines a model class for an employee.
+    /// </summary>
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+    }
+}
