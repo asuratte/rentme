33501b1b87fd2071c38c6b71604dcfb89d090a4e
Michael Bastarache <mb00147@my.westga.edu>
2022-03-13 14:12:37 -0400

    Created empty admin interface with logout functionality. Added methods to display admin information in labels.

---
+++ RentMe/View/AdminInterface.cs
@@ -0,0 +1,34 @@
+﻿using RentMe.Model;
+using System.Windows.Forms;
+
+namespace RentMe.View
+{
+    /// <summary>
+    /// Represents the main admin interface of the RentMe Application
+    /// </summary>
+    public partial class AdminInterface : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdminInterface"/> class.
+        /// </summary>
+        public AdminInterface()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sets the employee information on the interface.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        public void SetEmployee(Employee employee)
+        {
+            this.employeeNameLabel.Text = employee.FirstName + " " + employee.LastName;
+            this.employeeUsernameLabel.Text = employee.Username;
+        }
+
+        private void LogoutLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
