9379d66e3e488ac64436b0104a7525071d89ba4b
Michael Bastarache <mb00147@my.westga.edu>
2022-03-23 21:19:00 -0400

    Created controller and DAL for furniture_category with method to retrieve all category names

---
+++ RentMe/Controller/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using RentMe.DAL;
+using System;
+using System.Collections.Generic;
+
+namespace RentMe.Controller
+{
+    /// <summary>
+    /// Defines a controller class that mediates between the CategoryDAL and the view.
+    /// </summary>
+    public class CategoryController
+    {
+        private readonly CategoryDAL categoryDAL;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CategoryController"/> class.
+        /// </summary>
+        public CategoryController()
+        {
+            this.categoryDAL = new CategoryDAL();
+        }
+
+        /// <summary>
+        /// Retrieve list of all categories from the furniture category table in the database
+        /// </summary>
+        /// <returns>List of all furniture categories in the database</returns>
+        public List<String> GetAllCategories()
+        {
+            return this.categoryDAL.GetAllCategories();
+        }
+    }
+}
---
+++ RentMe/DAL/CategoryDAL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace RentMe.DAL
+{
+    /// <summary>
+    /// Data Access Layer for Furniture Categories
+    /// </summary>
+    public class CategoryDAL
+    {
+        /// <summary>
+        /// Retrieve list of all category names from the furniture category table in the database
+        /// </summary>
+        /// <returns>List of all furniture category names in the database</returns>
+        public List<String> GetAllCategories()
+        {
+            List<String> categoryList = new List<String>();
+
+            string selectStatement = "SELECT categoryName FROM Furniture_Category";
+            using (SqlConnection connection = RentMeDBConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            String categoryName = "";
+                            categoryName = reader["categoryName"].ToString();
+                            categoryList.Add(categoryName);
+                        }
+                    }
+                }
+            }
+            return categoryList;
+        }
+    }
+}
