e44fbea782abd2563989cb55f1639b731649c0f2
Amber Suratte <as00193@my.westga.edu>
2022-03-30 07:01:45 -0500

    Renamed columns for MemberLookupForm to be more descriptive.

--- RentMe/View/MemberLookupForm.Designer.cs
+++ RentMe/View/MemberLookupForm.Designer.cs
@@ -31,20 +31,20 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MemberLookupForm));
             this.memberDataGridView = new System.Windows.Forms.DataGridView();
-            this.cancelButton = new System.Windows.Forms.Button();
-            this.SelectMember = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.memberBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.dataGridViewMemberIDTextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewFirstNameTextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewLastNameTextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewPhoneTextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewDateOfBirthTextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewSexTextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewAddress1TextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewAddress2TextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewCityTextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewStateTextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewZipCodeTextBox = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SelectMember = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.memberDataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.memberBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -57,17 +57,17 @@
             this.memberDataGridView.AutoGenerateColumns = false;
             this.memberDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.memberDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dataGridViewTextBoxColumn1,
-            this.dataGridViewTextBoxColumn4,
-            this.dataGridViewTextBoxColumn5,
-            this.dataGridViewTextBoxColumn6,
-            this.dataGridViewTextBoxColumn3,
-            this.dataGridViewTextBoxColumn2,
-            this.dataGridViewTextBoxColumn7,
-            this.dataGridViewTextBoxColumn8,
-            this.dataGridViewTextBoxColumn9,
-            this.dataGridViewTextBoxColumn10,
-            this.dataGridViewTextBoxColumn11,
+            this.dataGridViewMemberIDTextBox,
+            this.dataGridViewFirstNameTextBox,
+            this.dataGridViewLastNameTextBox,
+            this.dataGridViewPhoneTextBox,
+            this.dataGridViewDateOfBirthTextBox,
+            this.dataGridViewSexTextBox,
+            this.dataGridViewAddress1TextBox,
+            this.dataGridViewAddress2TextBox,
+            this.dataGridViewCityTextBox,
+            this.dataGridViewStateTextBox,
+            this.dataGridViewZipCodeTextBox,
             this.SelectMember});
             this.memberDataGridView.DataSource = this.memberBindingSource;
             this.memberDataGridView.Location = new System.Drawing.Point(12, 12);
@@ -76,6 +76,10 @@
             this.memberDataGridView.Size = new System.Drawing.Size(659, 244);
             this.memberDataGridView.TabIndex = 2;
             this.memberDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.MemberDataGridViewCellContentClick);
+            // 
+            // memberBindingSource
+            // 
+            this.memberBindingSource.DataSource = typeof(RentMe.Model.Member);
             // 
             // cancelButton
             // 
@@ -92,6 +96,98 @@
             this.cancelButton.UseVisualStyleBackColor = false;
             this.cancelButton.Click += new System.EventHandler(this.CancelButtonOnClick);
             // 
+            // dataGridViewMemberIDTextBox
+            // 
+            this.dataGridViewMemberIDTextBox.DataPropertyName = "MemberID";
+            this.dataGridViewMemberIDTextBox.HeaderText = "Member ID";
+            this.dataGridViewMemberIDTextBox.Name = "dataGridViewMemberIDTextBox";
+            this.dataGridViewMemberIDTextBox.ReadOnly = true;
+            this.dataGridViewMemberIDTextBox.Width = 95;
+            // 
+            // dataGridViewFirstNameTextBox
+            // 
+            this.dataGridViewFirstNameTextBox.DataPropertyName = "FirstName";
+            this.dataGridViewFirstNameTextBox.HeaderText = "First Name";
+            this.dataGridViewFirstNameTextBox.Name = "dataGridViewFirstNameTextBox";
+            this.dataGridViewFirstNameTextBox.ReadOnly = true;
+            this.dataGridViewFirstNameTextBox.Width = 95;
+            // 
+            // dataGridViewLastNameTextBox
+            // 
+            this.dataGridViewLastNameTextBox.DataPropertyName = "LastName";
+            this.dataGridViewLastNameTextBox.HeaderText = "Last Name";
+            this.dataGridViewLastNameTextBox.Name = "dataGridViewLastNameTextBox";
+            this.dataGridViewLastNameTextBox.ReadOnly = true;
+            this.dataGridViewLastNameTextBox.Width = 95;
+            // 
+            // dataGridViewPhoneTextBox
+            // 
+            this.dataGridViewPhoneTextBox.DataPropertyName = "Phone";
+            this.dataGridViewPhoneTextBox.HeaderText = "Phone";
+            this.dataGridViewPhoneTextBox.Name = "dataGridViewPhoneTextBox";
+            this.dataGridViewPhoneTextBox.ReadOnly = true;
+            this.dataGridViewPhoneTextBox.Width = 95;
+            // 
+            // dataGridViewDateOfBirthTextBox
+            // 
+            this.dataGridViewDateOfBirthTextBox.DataPropertyName = "DateOfBirth";
+            this.dataGridViewDateOfBirthTextBox.HeaderText = "Date Of Birth";
+            this.dataGridViewDateOfBirthTextBox.Name = "dataGridViewDateOfBirthTextBox";
+            this.dataGridViewDateOfBirthTextBox.ReadOnly = true;
+            // 
+            // dataGridViewSexTextBox
+            // 
+            this.dataGridViewSexTextBox.DataPropertyName = "Sex";
+            this.dataGridViewSexTextBox.HeaderText = "Sex";
+            this.dataGridViewSexTextBox.Name = "dataGridViewSexTextBox";
+            this.dataGridViewSexTextBox.ReadOnly = true;
+            this.dataGridViewSexTextBox.Width = 35;
+            // 
+            // dataGridViewAddress1TextBox
+            // 
+            this.dataGridViewAddress1TextBox.DataPropertyName = "Address1";
+            this.dataGridViewAddress1TextBox.HeaderText = "Address 1";
+            this.dataGridViewAddress1TextBox.Name = "dataGridViewAddress1TextBox";
+            this.dataGridViewAddress1TextBox.ReadOnly = true;
+            this.dataGridViewAddress1TextBox.Visible = false;
+            this.dataGridViewAddress1TextBox.Width = 95;
+            // 
+            // dataGridViewAddress2TextBox
+            // 
+            this.dataGridViewAddress2TextBox.DataPropertyName = "Address2";
+            this.dataGridViewAddress2TextBox.HeaderText = "Address 2";
+            this.dataGridViewAddress2TextBox.Name = "dataGridViewAddress2TextBox";
+            this.dataGridViewAddress2TextBox.ReadOnly = true;
+            this.dataGridViewAddress2TextBox.Visible = false;
+            this.dataGridViewAddress2TextBox.Width = 65;
+            // 
+            // dataGridViewCityTextBox
+            // 
+            this.dataGridViewCityTextBox.DataPropertyName = "City";
+            this.dataGridViewCityTextBox.HeaderText = "City";
+            this.dataGridViewCityTextBox.Name = "dataGridViewCityTextBox";
+            this.dataGridViewCityTextBox.ReadOnly = true;
+            this.dataGridViewCityTextBox.Visible = false;
+            this.dataGridViewCityTextBox.Width = 65;
+            // 
+            // dataGridViewStateTextBox
+            // 
+            this.dataGridViewStateTextBox.DataPropertyName = "State";
+            this.dataGridViewStateTextBox.HeaderText = "State";
+            this.dataGridViewStateTextBox.Name = "dataGridViewStateTextBox";
+            this.dataGridViewStateTextBox.ReadOnly = true;
+            this.dataGridViewStateTextBox.Visible = false;
+            this.dataGridViewStateTextBox.Width = 45;
+            // 
+            // dataGridViewZipCodeTextBox
+            // 
+            this.dataGridViewZipCodeTextBox.DataPropertyName = "ZipCode";
+            this.dataGridViewZipCodeTextBox.HeaderText = "Zip Code";
+            this.dataGridViewZipCodeTextBox.Name = "dataGridViewZipCodeTextBox";
+            this.dataGridViewZipCodeTextBox.ReadOnly = true;
+            this.dataGridViewZipCodeTextBox.Visible = false;
+            this.dataGridViewZipCodeTextBox.Width = 85;
+            // 
             // SelectMember
             // 
             this.SelectMember.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -100,102 +196,6 @@
             this.SelectMember.ReadOnly = true;
             this.SelectMember.Text = "Select";
             this.SelectMember.UseColumnTextForButtonValue = true;
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this.dataGridViewTextBoxColumn1.DataPropertyName = "MemberID";
-            this.dataGridViewTextBoxColumn1.HeaderText = "Member ID";
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            this.dataGridViewTextBoxColumn1.ReadOnly = true;
-            this.dataGridViewTextBoxColumn1.Width = 95;
-            // 
-            // dataGridViewTextBoxColumn4
-            // 
-            this.dataGridViewTextBoxColumn4.DataPropertyName = "FirstName";
-            this.dataGridViewTextBoxColumn4.HeaderText = "First Name";
-            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            this.dataGridViewTextBoxColumn4.ReadOnly = true;
-            this.dataGridViewTextBoxColumn4.Width = 95;
-            // 
-            // dataGridViewTextBoxColumn5
-            // 
-            this.dataGridViewTextBoxColumn5.DataPropertyName = "LastName";
-            this.dataGridViewTextBoxColumn5.HeaderText = "Last Name";
-            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
-            this.dataGridViewTextBoxColumn5.ReadOnly = true;
-            this.dataGridViewTextBoxColumn5.Width = 95;
-            // 
-            // dataGridViewTextBoxColumn6
-            // 
-            this.dataGridViewTextBoxColumn6.DataPropertyName = "Phone";
-            this.dataGridViewTextBoxColumn6.HeaderText = "Phone";
-            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
-            this.dataGridViewTextBoxColumn6.ReadOnly = true;
-            this.dataGridViewTextBoxColumn6.Width = 95;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this.dataGridViewTextBoxColumn3.DataPropertyName = "DateOfBirth";
-            this.dataGridViewTextBoxColumn3.HeaderText = "Date Of Birth";
-            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            this.dataGridViewTextBoxColumn3.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this.dataGridViewTextBoxColumn2.DataPropertyName = "Sex";
-            this.dataGridViewTextBoxColumn2.HeaderText = "Sex";
-            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            this.dataGridViewTextBoxColumn2.ReadOnly = true;
-            this.dataGridViewTextBoxColumn2.Width = 35;
-            // 
-            // dataGridViewTextBoxColumn7
-            // 
-            this.dataGridViewTextBoxColumn7.DataPropertyName = "Address1";
-            this.dataGridViewTextBoxColumn7.HeaderText = "Address 1";
-            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
-            this.dataGridViewTextBoxColumn7.ReadOnly = true;
-            this.dataGridViewTextBoxColumn7.Visible = false;
-            this.dataGridViewTextBoxColumn7.Width = 95;
-            // 
-            // dataGridViewTextBoxColumn8
-            // 
-            this.dataGridViewTextBoxColumn8.DataPropertyName = "Address2";
-            this.dataGridViewTextBoxColumn8.HeaderText = "Address 2";
-            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
-            this.dataGridViewTextBoxColumn8.ReadOnly = true;
-            this.dataGridViewTextBoxColumn8.Visible = false;
-            this.dataGridViewTextBoxColumn8.Width = 65;
-            // 
-            // dataGridViewTextBoxColumn9
-            // 
-            this.dataGridViewTextBoxColumn9.DataPropertyName = "City";
-            this.dataGridViewTextBoxColumn9.HeaderText = "City";
-            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
-            this.dataGridViewTextBoxColumn9.ReadOnly = true;
-            this.dataGridViewTextBoxColumn9.Visible = false;
-            this.dataGridViewTextBoxColumn9.Width = 65;
-            // 
-            // dataGridViewTextBoxColumn10
-            // 
-            this.dataGridViewTextBoxColumn10.DataPropertyName = "State";
-            this.dataGridViewTextBoxColumn10.HeaderText = "State";
-            this.dataGridViewTextBoxColumn10.Name = "dataGridViewTextBoxColumn10";
-            this.dataGridViewTextBoxColumn10.ReadOnly = true;
-            this.dataGridViewTextBoxColumn10.Visible = false;
-            this.dataGridViewTextBoxColumn10.Width = 45;
-            // 
-            // dataGridViewTextBoxColumn11
-            // 
-            this.dataGridViewTextBoxColumn11.DataPropertyName = "ZipCode";
-            this.dataGridViewTextBoxColumn11.HeaderText = "Zip Code";
-            this.dataGridViewTextBoxColumn11.Name = "dataGridViewTextBoxColumn11";
-            this.dataGridViewTextBoxColumn11.ReadOnly = true;
-            this.dataGridViewTextBoxColumn11.Visible = false;
-            this.dataGridViewTextBoxColumn11.Width = 85;
-            // 
-            // memberBindingSource
-            // 
-            this.memberBindingSource.DataSource = typeof(RentMe.Model.Member);
             // 
             // MemberLookupForm
             // 
@@ -222,17 +222,17 @@
         private System.Windows.Forms.BindingSource memberBindingSource;
         private System.Windows.Forms.DataGridView memberDataGridView;
         private System.Windows.Forms.Button cancelButton;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn10;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewMemberIDTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewFirstNameTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewLastNameTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewPhoneTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewDateOfBirthTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewSexTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewAddress1TextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewAddress2TextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewCityTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewStateTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewZipCodeTextBox;
         private System.Windows.Forms.DataGridViewButtonColumn SelectMember;
     }
 }
