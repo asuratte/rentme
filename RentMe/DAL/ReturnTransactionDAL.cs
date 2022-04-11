b17726492d0114fe264d03a8b4334fb3c0a37962
Amber Suratte <as00193@my.westga.edu>
2022-04-11 07:57:15 -0500

    Created ReturnTransactionDAL class with AddReturnTransactionAndItems method for inserting a return transaction and associated items into the database.

---
+++ RentMe/DAL/ReturnTransactionDAL.cs
@@ -0,0 +1,79 @@
+﻿using RentMe.Model;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace RentMe.DAL
+{
+    /// <summary>
+    /// Data Access Layer for Return Transaction
+    /// </summary>
+    public class ReturnTransactionDAL
+    {
+        /// <summary>
+        /// Adds the return transaction and items to the database.
+        /// </summary>
+        /// <param name="memberID">The member identifier.</param>
+        /// <param name="employeeID">The employee identifier.</param>
+        /// <param name="returnedItemsListView">The returned items ListView.</param>
+        /// <returns>The return transaction ID</returns>
+        public int AddReturnTransactionAndItems(int memberID, int employeeID, ListView returnedItemsListView)
+        {
+            string insertRentalTransactionStatement =
+                @"INSERT INTO rental_transaction (memberID, employeeID, returnDate) 
+                VALUES (@MemberID, @EmployeeID, @ReturnDate)";
+
+            string selectTransactionID = "SELECT @@Identity";
+
+            string insertRentalItemStatement =
+                @"INSERT INTO rental_item (transactionID, rentalTransactionID, furnitureID, quantity) 
+                VALUES (@TransactionID, @RentalTransactionID, @FurnitureID, @Quantity)";
+
+            using (SqlConnection connection = RentMeDBConnection.GetConnection())
+            {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+                int transactionID = 0;
+                try
+                {
+                    using (SqlCommand insertCommand = new SqlCommand(insertRentalTransactionStatement, connection))
+                    {
+                        insertCommand.Parameters.Add("@MemberID", System.Data.SqlDbType.Int);
+                        insertCommand.Parameters["@MemberID"].Value = memberID;
+                        insertCommand.Parameters.Add("@EmployeeID", System.Data.SqlDbType.Int);
+                        insertCommand.Parameters["@EmployeeID"].Value = employeeID;
+                        insertCommand.Parameters.Add("@ReturnDate", System.Data.SqlDbType.DateTime);
+                        insertCommand.Parameters["@ReturnDate"].Value = DateTime.Now;
+                        insertCommand.ExecuteNonQuery();
+                    }
+                    using (SqlCommand selectCommand = new SqlCommand(selectTransactionID, connection))
+                    {
+                        transactionID = selectCommand.ExecuteNonQuery();
+                    }
+                    using (SqlCommand insertCommand = new SqlCommand(insertRentalItemStatement, connection))
+                    {
+                        for (int i = 0; i < returnedItemsListView.Items.Count; i++)
+                        {
+                            ReturnItem theReturnedItem = (ReturnItem)returnedItemsListView.Items[i].Tag;
+                            insertCommand.Parameters.Add("@TransactionID", System.Data.SqlDbType.Int);
+                            insertCommand.Parameters["@TransactionID"].Value = transactionID;
+                            insertCommand.Parameters.Add("@RentalTransactionID", System.Data.SqlDbType.Int);
+                            insertCommand.Parameters["@RentalTransactionID"].Value = theReturnedItem.RentalTransactionID;
+                            insertCommand.Parameters.Add("@FurnitureID", System.Data.SqlDbType.VarChar);
+                            insertCommand.Parameters["@FurnitureID"].Value = theReturnedItem.FurnitureID;
+                            insertCommand.Parameters.Add("@Quantity", System.Data.SqlDbType.Int);
+                            insertCommand.Parameters["@Quantity"].Value = theReturnedItem.Quantity;
+                            insertCommand.ExecuteNonQuery();
+                        }
+                    }
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                }
+                return transactionID;
+            }
+        }
+    }
+}
