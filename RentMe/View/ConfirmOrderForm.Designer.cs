7caba641236d126ca81bf8222e30d8011b0d3986
Michael Bastarache <mb00147@my.westga.edu>
2022-04-13 16:29:33 -0400

    Created Confirm Order Form and added controls to design

---
+++ RentMe/View/ConfirmOrderForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace RentMe.View
+{
+    partial class ConfirmOrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.furnitureIDColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.furnitureNameColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.quantityColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dueDateColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.confirmOrderDetailsLabel = new System.Windows.Forms.Label();
+            this.submitOrderButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalValueLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.furnitureIDColumnHeader,
+            this.furnitureNameColumnHeader,
+            this.quantityColumnHeader,
+            this.dueDateColumnHeader});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 34);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(484, 206);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // furnitureIDColumnHeader
+            // 
+            this.furnitureIDColumnHeader.Text = "Furniture ID";
+            // 
+            // furnitureNameColumnHeader
+            // 
+            this.furnitureNameColumnHeader.Text = "Furniture Name";
+            // 
+            // quantityColumnHeader
+            // 
+            this.quantityColumnHeader.Text = "Quantity";
+            // 
+            // dueDateColumnHeader
+            // 
+            this.dueDateColumnHeader.Text = "Due Date";
+            // 
+            // confirmOrderDetailsLabel
+            // 
+            this.confirmOrderDetailsLabel.AutoSize = true;
+            this.confirmOrderDetailsLabel.Location = new System.Drawing.Point(8, 9);
+            this.confirmOrderDetailsLabel.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.confirmOrderDetailsLabel.Name = "confirmOrderDetailsLabel";
+            this.confirmOrderDetailsLabel.Size = new System.Drawing.Size(293, 19);
+            this.confirmOrderDetailsLabel.TabIndex = 1;
+            this.confirmOrderDetailsLabel.Text = "Please confirm order details before submitting.";
+            // 
+            // submitOrderButton
+            // 
+            this.submitOrderButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(103)))), ((int)(((byte)(136)))));
+            this.submitOrderButton.FlatAppearance.BorderSize = 0;
+            this.submitOrderButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.submitOrderButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitOrderButton.ForeColor = System.Drawing.Color.White;
+            this.submitOrderButton.Location = new System.Drawing.Point(197, 303);
+            this.submitOrderButton.Name = "submitOrderButton";
+            this.submitOrderButton.Size = new System.Drawing.Size(168, 27);
+            this.submitOrderButton.TabIndex = 19;
+            this.submitOrderButton.Text = "Confirm && Submit";
+            this.submitOrderButton.UseVisualStyleBackColor = false;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.BackColor = System.Drawing.Color.DimGray;
+            this.cancelButton.FlatAppearance.BorderSize = 0;
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.ForeColor = System.Drawing.Color.White;
+            this.cancelButton.Location = new System.Drawing.Point(386, 303);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(110, 27);
+            this.cancelButton.TabIndex = 20;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(357, 260);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(41, 19);
+            this.totalLabel.TabIndex = 21;
+            this.totalLabel.Text = "Total:";
+            // 
+            // totalValueLabel
+            // 
+            this.totalValueLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalValueLabel.Location = new System.Drawing.Point(404, 260);
+            this.totalValueLabel.Name = "totalValueLabel";
+            this.totalValueLabel.Size = new System.Drawing.Size(92, 19);
+            this.totalValueLabel.TabIndex = 22;
+            // 
+            // ConfirmOrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 342);
+            this.Controls.Add(this.totalValueLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.submitOrderButton);
+            this.Controls.Add(this.confirmOrderDetailsLabel);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "ConfirmOrderForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Confirm Order";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader furnitureIDColumnHeader;
+        private System.Windows.Forms.ColumnHeader furnitureNameColumnHeader;
+        private System.Windows.Forms.ColumnHeader quantityColumnHeader;
+        private System.Windows.Forms.ColumnHeader dueDateColumnHeader;
+        private System.Windows.Forms.Label confirmOrderDetailsLabel;
+        private System.Windows.Forms.Button submitOrderButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label totalValueLabel;
+    }
+}
