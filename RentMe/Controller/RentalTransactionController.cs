51ea568cf7f2f6f3d0c7a5e8603b8e2c35b3dead
Michael Bastarache <mb00147@my.westga.edu>
2022-04-13 19:10:58 -0400

    Created controller and DAL for rental transactions, with methods to add rental transaction and items to DB.

---
+++ RentMe/Controller/RentalTransactionController.cs
@@ -0,0 +1,55 @@
+﻿using RentMe.DAL;
+using RentMe.Model;
+using System;
+using System.Collections.Generic;
+
+namespace RentMe.Controller
+{
+    /// <summary>
+    /// Defines a controller class that mediates between the RentalTransaction DAL and the view.
+    /// </summary>
+    public class RentalTransactionController
+    {
+        private readonly RentalTransactionDAL rentalTransactionDAL;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RentalTransactionController"/> class.
+        /// </summary>
+        public RentalTransactionController()
+        {
+            this.rentalTransactionDAL = new RentalTransactionDAL();
+        }
+
+        /// <summary>
+        /// Adds the return transaction and items to the database.
+        /// </summary>
+        /// <param name="memberID">The member identifier.</param>
+        /// <param name="employeeID">The employee identifier.</param>
+        /// <param name="rentalItemsList">The rental items list.</param>
+        /// <param name="dueDate">The due date.</param>
+        /// <returns>The rental transaction ID.</returns>
+        /// <exception cref="System.ArgumentException">
+        /// Member ID must be a positive whole number.
+        /// or
+        /// Employee ID must be a positive whole number.
+        /// or
+        /// There must be at least one furniture item in the list.
+        /// </exception>
+        public int AddReturnTransactionAndItems(int memberID, int employeeID, List<RentalItem> rentalItemsList, DateTime dueDate)
+        {
+            if (memberID <= 0)
+            {
+                throw new ArgumentException("Member ID must be a positive whole number.");
+            }
+            if (employeeID <= 0)
+            {
+                throw new ArgumentException("Employee ID must be a positive whole number.");
+            }
+            if (rentalItemsList.Count < 1 || rentalItemsList == null)
+            {
+                throw new ArgumentException("There must be at least one furniture item in the list.");
+            }
+            return this.rentalTransactionDAL.AddRentalTransactionAndItems(memberID, employeeID, rentalItemsList, dueDate);
+        }
+    }
+}
---
+++ RentMe/DAL/RentalTransactionDAL.cs
@@ -0,0 +1,101 @@
+﻿using RentMe.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace RentMe.DAL
+{
+    /// <summary>
+    /// Data Access Layer for Rental Transaction
+    /// </summary>
+    public class RentalTransactionDAL
+    {
+        /// <summary>
+        /// Adds the rental transaction and items to the database.
+        /// </summary>
+        /// <param name="memberID">The member identifier.</param>
+        /// <param name="employeeID">The employee identifier.</param>
+        /// <param name="rentalItemsList">The rental items list.</param>
+        /// <param name="dueDate">The due date.</param>
+        /// <returns>The rental transaction ID.</returns>
+        public int AddRentalTransactionAndItems(int memberID, int employeeID, List<RentalItem> rentalItemsList, DateTime dueDate)
+        {
+            string insertRentalTransactionStatement =
+                @"INSERT INTO rental_transaction (memberID, employeeID, rentalDate, dueDate) 
+                VALUES (@MemberID, @EmployeeID, @RentalDate, @DueDate)";
+
+            string selectTransactionIDStatement = "SELECT @@Identity";
+
+            string insertRentalItemStatement =
+                @"INSERT INTO rental_item (transactionID, furnitureID, quantity) 
+                VALUES (@TransactionID, @FurnitureID, @Quantity)";
+
+            string updateFurnitureInventoryStatement =
+                @"UPDATE furniture SET
+                   totalQuantity = totalQuantity - @QuantityRented
+                WHERE furnitureID = @FurnitureID";
+
+            using (SqlConnection connection = RentMeDBConnection.GetConnection())
+            {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+                int transactionID = 0;
+                try
+                {
+                    using (SqlCommand insertCommand = new SqlCommand(insertRentalTransactionStatement, connection))
+                    {
+                        insertCommand.Transaction = transaction;
+                        insertCommand.Parameters.Add("@MemberID", System.Data.SqlDbType.Int);
+                        insertCommand.Parameters["@MemberID"].Value = memberID;
+                        insertCommand.Parameters.Add("@EmployeeID", System.Data.SqlDbType.Int);
+                        insertCommand.Parameters["@EmployeeID"].Value = employeeID;
+                        insertCommand.Parameters.Add("@RentalDate", System.Data.SqlDbType.DateTime);
+                        insertCommand.Parameters["@RentalDate"].Value = DateTime.Now;
+                        insertCommand.Parameters.Add("@DueDate", System.Data.SqlDbType.DateTime);
+                        insertCommand.Parameters["@DueDate"].Value = dueDate;
+                        insertCommand.ExecuteNonQuery();
+                    }
+                    using (SqlCommand selectCommand = new SqlCommand(selectTransactionIDStatement, connection))
+                    {
+                        selectCommand.Transaction = transaction;
+                        transactionID = Convert.ToInt32(selectCommand.ExecuteScalar());
+                    }
+                    using (SqlCommand insertCommand = new SqlCommand(insertRentalItemStatement, connection))
+                    {
+                        insertCommand.Transaction = transaction;
+                        foreach (RentalItem theRentalItem in rentalItemsList)
+                        {
+                            insertCommand.Parameters.Add("@TransactionID", System.Data.SqlDbType.Int);
+                            insertCommand.Parameters["@TransactionID"].Value = transactionID;
+                            insertCommand.Parameters.Add("@FurnitureID", System.Data.SqlDbType.VarChar);
+                            insertCommand.Parameters["@FurnitureID"].Value = theRentalItem.FurnitureID;
+                            insertCommand.Parameters.Add("@Quantity", System.Data.SqlDbType.Int);
+                            insertCommand.Parameters["@Quantity"].Value = theRentalItem.Quantity;
+                            insertCommand.ExecuteNonQuery();
+                            insertCommand.Parameters.Clear();
+                        }
+                    }
+                    using (SqlCommand updateCommand = new SqlCommand(updateFurnitureInventoryStatement, connection))
+                    {
+                        updateCommand.Transaction = transaction;
+                        foreach (RentalItem theRentalItem in rentalItemsList)
+                        {
+                            updateCommand.Parameters.Add("@FurnitureID", System.Data.SqlDbType.VarChar);
+                            updateCommand.Parameters["@FurnitureID"].Value = theRentalItem.FurnitureID;
+                            updateCommand.Parameters.Add("@QuantityRented", System.Data.SqlDbType.Int);
+                            updateCommand.Parameters["@QuantityRented"].Value = theRentalItem.Quantity;
+                            updateCommand.ExecuteNonQuery();
+                            updateCommand.Parameters.Clear();
+                        }
+                    }
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                }
+                return transactionID;
+            }
+        }
+    }
+}
