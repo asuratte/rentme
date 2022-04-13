d5777b23f03fa1157dc03cbc5e0adbd9255b89ce
Michael Bastarache <mb00147@my.westga.edu>
2022-04-13 19:36:27 -0400

    Created model class for rental transaction

---
+++ RentMe/Model/RentalTransaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RentMe.Model
+{
+    /// <summary>
+    /// Defines a model class for a Rental Transaction
+    /// </summary>
+    public class RentalTransaction
+    {
+        public int TransactionID { get; set; }
+        public int MemberID { get; set; }
+        public int EmployeeID { get; set; }
+        public DateTime RentalDate { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
