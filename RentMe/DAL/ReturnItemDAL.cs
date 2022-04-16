f96e3180e541d9781a7a5ce6efab4beb7c3d51fe
Amber Suratte <as00193@my.westga.edu>
2022-04-16 12:35:18 -0500

    Modified ReturnItem model to include RentalRate and TransactionID properties. Added ReturnItemDAL and ReturnItemController and added method GetReturnItemsByTransactionID for getting a list of return items by transaction ID.

---
+++ RentMe/DAL/ReturnItemDAL.cs
@@ -0,0 +1,55 @@
+﻿using RentMe.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace RentMe.DAL
+{
+    /// <summary>
+    /// Data Access Layer for Return Items
+    /// </summary>
+    public class ReturnItemDAL
+    {
+        /// <summary>
+        /// Gets the return items by transactionID.
+        /// </summary>
+        /// <param name="transactionID">The transaction identifier.</param>
+        /// <returns>List of return items for a given transaction ID</returns>
+        public List<ReturnItem> GetReturnItemsByTransactionID(int transactionID)
+        {
+            string selectStatement =
+            @"SELECT ri.quantity, ri.furnitureID, f.name, f.rentalRate
+              FROM return_item ri
+              JOIN furniture f ON ri.furnitureID = f.furnitureID
+              WHERE transactionID = @TransactionID";
+
+            List<ReturnItem> theReturnItemList = new List<ReturnItem>();
+
+            using (SqlConnection connection = RentMeDBConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.Add("@TransactionID", System.Data.SqlDbType.Int);
+                    selectCommand.Parameters["@TransactionID"].Value = transactionID;
+
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ReturnItem theReturnItem = new ReturnItem();
+                            theReturnItem.TransactionID = transactionID;
+                            theReturnItem.FurnitureID = reader["furnitureID"].ToString();
+                            theReturnItem.FurnitureName = reader["name"].ToString();
+                            theReturnItem.Quantity = Convert.ToInt32(reader["quantity"]);
+                            theReturnItem.RentalRate = Convert.ToDecimal(reader["rentalRate"]);
+                            theReturnItemList.Add(theReturnItem);
+                        }
+                    }
+                }
+            }
+            return theReturnItemList;
+        }
+    }
+}
--- RentMe/Model/ReturnItem.cs
+++ RentMe/Model/ReturnItem.cs
@@ -5,11 +5,13 @@
     /// </summary>
     public class ReturnItem
     {
+        public int TransactionID { get; set; }
         public int RentalTransactionID { get; set; }
         public string FurnitureID { get; set; }
         public string FurnitureName { get; set; }
         public int Quantity { get; set; }
         public decimal ItemTotal { get; set; }
         public string ItemTotalDisplay { get; set; }
+        public decimal RentalRate { get; set; }
     }
 }
